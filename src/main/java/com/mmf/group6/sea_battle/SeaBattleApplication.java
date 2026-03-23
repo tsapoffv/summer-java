be69653d8d2a546c19b5db5a511062b5ce0e16fb
Aleksandr Kudzelka <igortsapovf@proton.me>
2026-03-23 19:53:46 +0300

    Project created

---
+++ src/main/java/com/mmf/group6/sea_battle/SeaBattleApplication.java
@@ -0,0 +1,13 @@
+package com.mmf.group6.sea_battle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmf/group6/sea_battle/SeaBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmf.group6.sea_battle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
