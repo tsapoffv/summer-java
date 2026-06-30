6f967c0c4768e3fcabc9eb7c2ca0212a661accf5
Maksim23060 <maxim.martin2007@gmail.com>
2026-06-30 10:21:21 +0300

    Update Constants.java
    CELL_SIZE: 48->40

--- src/client/utils/Constants.java
+++ src/client/utils/Constants.java
@@ -13,7 +13,7 @@ public final class Constants {
     public static final int BOARD_SIZE = 10;
 
     /** Размер клетки в пикселях */
-    public static final int CELL_SIZE = 48;
+    public static final int CELL_SIZE = 40;
 
     /** Размеры кораблей для расстановки */
     public static final int[] SHIP_SIZES = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
